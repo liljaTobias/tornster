87fd70064a0214536c1f6b869c11ceb682513182
Tobias Lilja <lilja.tobias94@gmail.com>
2026-01-15 19:52:59 +0100

    ui deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css"],
-  ui: {
-    colors: {
-      primary: "blue",
-      gray: "slate",
-    },
-  },
+  vite: { plugins: [tailwindcss()] },
 });
