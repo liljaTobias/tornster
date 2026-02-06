23850f02a3b8fa5cb9224e6d92ca4ddcf6febe77
Looneguy <89279463+Looneguy@users.noreply.github.com>
2026-02-06 12:41:58 +0100

    feat: Added faction page to test dynamic routing with "NuxtLink"
    - Moved FactionCard component to faction page
    - Refactored FactionCard

--- app/components/FactionCard.vue
+++ app/components/FactionCard.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import type { Faction } from "#shared/types/torn/attack";
 
+const { factionId } = defineProps<{
+  factionId:string
+}>()
+
 const { data: basicInfo } = await useFetch<Faction>(
-  "/api/torn/faction/basic?ID=53421",
+  `/api/torn/faction/basic?ID=${factionId}`,
   {
     server: true,
   },
@@ -12,13 +16,15 @@ const { data: basicInfo } = await useFetch<Faction>(
 <template>
   <UCard>
     <template #header>
-      <h2 class="text-lg font-semibold">Faction Basic Info</h2>
+      <h2 class="text-lg font-semibold">Faction Basic Info - {{ basicInfo?.name }}</h2>
     </template>
 
     <div v-if="basicInfo" class="space-y-4">
       <ul>
-        <li v-for="(value, key) in basicInfo">
-          <strong>{{ key }}:</strong> {{ value }}
+        <li v-for="(value, key) in basicInfo"
+        :key="key"
+        >
+          <strong v-if="key !== 'name'">{{ key }}:</strong> {{ value }}
         </li>
       </ul>
     </div>
---
+++ app/pages/faction/[id].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const factionId = String(route.params.id);
+</script>
+<template>
+    <FactionCard :factionId="factionId"/>
+</template>
